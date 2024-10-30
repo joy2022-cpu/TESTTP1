43defd3a002055d5da8e3620064f8483302a5d86
ytembe.tchongouang <ytembe.tchongouang@gmail.com>
2024-10-30 12:30:23 +0100

    mon premier commit

---
+++ src/main/java/com/example/tptestu1_ytembe/Tptestu1YtembeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tptestu1_ytembe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tptestu1YtembeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tptestu1YtembeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tptestu1_ytembe/Tptestu1YtembeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tptestu1_ytembe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tptestu1YtembeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
